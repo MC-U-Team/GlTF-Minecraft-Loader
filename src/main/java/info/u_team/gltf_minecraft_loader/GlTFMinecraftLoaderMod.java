8bf6cbc228d469e3091172df91d3018f2f0dd8c2
HyCraftHD <hycrafthd@live.de>
2022-01-20 17:13:43 +0100

    Add basic mod

---
+++ src/main/java/info/u_team/gltf_minecraft_loader/GlTFMinecraftLoaderMod.java
@@ -0,0 +1,13 @@
+package info.u_team.gltf_minecraft_loader;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(GlTFMinecraftLoaderMod.MODID)
+public class GlTFMinecraftLoaderMod {
+	
+	public static final String MODID = "gltfminecraftloader";
+	
+	public GlTFMinecraftLoaderMod() {
+	}
+	
+}
