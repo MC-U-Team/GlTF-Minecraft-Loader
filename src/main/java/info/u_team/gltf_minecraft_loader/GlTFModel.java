de30a10bb8f713195a4f14502bc9cb00b5af6144
MrTroble <nifroh56@gmail.com>
2022-01-24 19:16:11 +0100

    Update

--- src/main/java/info/u_team/gltf_minecraft_loader/GlTFModel.java
+++ src/main/java/info/u_team/gltf_minecraft_loader/GlTFModel.java
@@ -1,8 +1,8 @@
 package info.u_team.gltf_minecraft_loader;
 
 import static info.u_team.gltf_parser.GlTFUtil.GLTF_COMPONENT_TYPE_UINT;
-import static info.u_team.gltf_parser.GlTFUtil.equalsOf;
-import static info.u_team.gltf_parser.GlTFUtil.of;
+import static info.u_team.gltf_parser.GlTFUtil.equalsOptional;
+import static info.u_team.gltf_parser.GlTFUtil.getOptionalEntry;
 
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
@@ -19,7 +19,6 @@ import java.util.function.Supplier;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Maps;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
-import com.mojang.blaze3d.vertex.VertexFormatElement;
 import com.mojang.datafixers.util.Pair;
 
 import info.u_team.gltf_parser.GlTFParser;
@@ -49,6 +48,7 @@ public class GlTFModel implements IModelGeometry<GlTFModel> {
 	
 	private static final HashMap<String, Integer> STRING_TO_ELEMENT = new HashMap<>();
 	private static final HashMap<String, Integer> STRING_TO_SIZE = new HashMap<>();
+	private static final HashMap<String, float[]> STRING_TO_DEFAULT = new HashMap<>();
 	private static final int POSITION;
 	
 	public GlTFModel(GlTFParser parser, GlTF gltf, Collection<Material> materials) {
@@ -63,22 +63,34 @@ public class GlTFModel implements IModelGeometry<GlTFModel> {
 	}
 	
 	static {
-		final HashMap<VertexFormatElement.Usage, String> elements = Maps.newHashMap();
-		elements.put(VertexFormatElement.Usage.POSITION, "POSITION");
-		elements.put(VertexFormatElement.Usage.UV, "TEXCOORD_0");
-		elements.put(VertexFormatElement.Usage.NORMAL, "NORMAL");
-		elements.put(VertexFormatElement.Usage.COLOR, "COLOR");
+		final HashMap<String, String> elements = Maps.newHashMap();
+		elements.put("Position", "POSITION");
+		elements.put("UV0", "TEXCOORD_0");
+		elements.put("UV1", "TEXCOORD_1");
+		elements.put("UV2", "TEXCOORD_1");
+		elements.put("Normal", "NORMAL");
+		elements.put("Color", "COLOR");
+		elements.put("Padding", "__UNUSED");
 		
 		STRING_TO_SIZE.put("POSITION", 3);
 		STRING_TO_SIZE.put("TEXCOORD_0", 2);
 		STRING_TO_SIZE.put("NORMAL", 3);
 		STRING_TO_SIZE.put("COLOR", 4);
+		STRING_TO_SIZE.put("TEXCOORD_1", 2);
+		STRING_TO_SIZE.put("__UNUSED", 0);
 		
-		final ImmutableList<VertexFormatElement> formats = DefaultVertexFormat.BLOCK.getElements();
+		STRING_TO_DEFAULT.put("POSITION", new float[] { 0f, 0f, 0f, 1f });
+		STRING_TO_DEFAULT.put("TEXCOORD_0", new float[] { 0f, 0f, 0f, 0f });
+		STRING_TO_DEFAULT.put("NORMAL", new float[] { 0f, 0f, 0f, 1f });
+		STRING_TO_DEFAULT.put("COLOR", new float[] { 1f, 1f, 1f, 1f });
+		STRING_TO_DEFAULT.put("TEXCOORD_1", new float[] { 0f, 0f, 0f, 0f });
+		STRING_TO_DEFAULT.put("__UNUSED", new float[] { 0f, 0f, 0f, 0f });
+		
+		final ImmutableList<String> formats = DefaultVertexFormat.BLOCK.getElementAttributeNames();
 		for (int j = 0; j < formats.size(); j++) {
-			final VertexFormatElement element = formats.get(j);
-			if (elements.containsKey(element.getUsage()))
-				STRING_TO_ELEMENT.put(elements.get(element.getUsage()), j);
+			final String element = formats.get(j);
+			if (elements.containsKey(element))
+				STRING_TO_ELEMENT.put(elements.get(element), j);
 		}
 		POSITION = STRING_TO_ELEMENT.get("POSITION");
 		STRING_TO_ELEMENT.remove("POSITION");
@@ -93,11 +105,13 @@ public class GlTFModel implements IModelGeometry<GlTFModel> {
 			for (int i = 0; i < indices.length; i++) {
 				indices[i] = indexBuffer.getAsInt();
 			}
+			final int position = vertexBuffer.position();
 			Arrays.stream(indices).distinct().forEachOrdered(index -> {
-				final int streamPos = index * 3 + vertexBuffer.position();
-				vertexConsumer.put(POSITION, vertexBuffer.get(streamPos), vertexBuffer.get(streamPos + 1), vertexBuffer.get(streamPos + 2));
+				final int streamPos = index * 3 + position;
+				vertexConsumer.put(POSITION, vertexBuffer.get(streamPos), vertexBuffer.get(streamPos + 1), vertexBuffer.get(streamPos + 2), 1f);
 			});
 			applyAttributes(attributes, vertexConsumer, indices);
+			continue;
 		}
 		// TODO Interpolate for non-powers of 6
 		for (int i = 0; i < rest; i++) {
@@ -111,9 +125,11 @@ public class GlTFModel implements IModelGeometry<GlTFModel> {
 		for (int j = 0; j < divider; j++) {
 			final IVertexConsumer vertexConsumer = vertexConsumerSupplier.get();
 			for (int i = 0; i < 4; i++) {
-				vertexConsumer.put(POSITION, buffer.getFloat(), buffer.getFloat(), buffer.getFloat());
+				vertexConsumer.put(POSITION, buffer.getFloat(), buffer.getFloat(), buffer.getFloat(), 1f);
 			}
-			applyAttributes(attributes, vertexConsumer);
+			final int offset = j * 4;
+			applyAttributes(attributes, vertexConsumer, new int[] { 0 + offset, 1 + offset, 2 + offset, 3 + offset });
+			continue;
 		}
 		// TODO Interpolate for non-powers of 4
 		for (int i = 0; i < rest; i++) {
@@ -123,44 +139,42 @@ public class GlTFModel implements IModelGeometry<GlTFModel> {
 		}
 	}
 	
-	private void applyAttributes(final Map<String, ByteBuffer> attributes, IVertexConsumer consumer) {
-		STRING_TO_ELEMENT.forEach((name, element) -> {
-			final ByteBuffer buffer = attributes.get(name);
-			float[] data = new float[STRING_TO_SIZE.get(name)];
-			for (int i = 0; i < data.length; i++) {
-				data[i] = buffer.getFloat();
-			}
-			consumer.put(element, data);
-		});
-	}
-	
 	private void applyAttributes(final Map<String, ByteBuffer> attributes, IVertexConsumer consumer, int[] indexBuffer) {
 		Arrays.stream(indexBuffer).distinct().forEachOrdered(index -> {
 			STRING_TO_ELEMENT.forEach((name, element) -> {
+				if (!attributes.containsKey(name)){
+					consumer.put(element, STRING_TO_DEFAULT.get(name));
+					return;
+				}
 				final ByteBuffer buffer = attributes.get(name);
-				float[] data = new float[STRING_TO_SIZE.get(name)];
-				final int offset = index * data.length * 4 + buffer.position();
-				for (int i = 0; i < data.length; i++) {
+				float[] data = new float[4];
+				final int readLength = STRING_TO_SIZE.get(name);
+				final int offset = index * readLength * 4 + buffer.position();
+				for (int i = 0; i < readLength; i++) {
 					data[i] = buffer.getFloat(offset + i);
+				}
+				for (int i = readLength; i < data.length; i++) {
+					data[i] = 0;
 				}
 				consumer.put(element, data);
 			});
 		});
+		return;
 	}
 	
 	private void addNode(final Node node, final Supplier<IVertexConsumer> vertexConsumerSupplier) {
-		of(this.gltf.getMeshes(), node.getMesh()).ifPresent(mesh -> {
+		getOptionalEntry(this.gltf.getMeshes(), node.getMesh()).ifPresent(mesh -> {
 			mesh.getPrimitives().forEach(primitive -> {
 				final Map<String, Number> attributes = (Map<String, Number>) primitive.getAttributes();
-				final Accessor accessor = of(gltf.getAccessors(), attributes.get("POSITION")).get();
+				final Accessor accessor = getOptionalEntry(gltf.getAccessors(), attributes.get("POSITION")).get();
 				final ByteBuffer buffer = this.parser.getData(accessor);
-				final Optional<Accessor> indices = of(gltf.getAccessors(), primitive.getIndices());
+				final Optional<Accessor> indices = getOptionalEntry(gltf.getAccessors(), primitive.getIndices());
 				final Map<String, ByteBuffer> attributesBuffer = new HashMap<>();
-				attributes.forEach((name, id) -> of(gltf.getAccessors(), id).ifPresent(access -> attributesBuffer.put(name, parser.getData(access))));
+				attributes.forEach((name, id) -> getOptionalEntry(gltf.getAccessors(), id).ifPresent(access -> attributesBuffer.put(name, parser.getData(access))));
 				if (indices.isPresent()) {
 					final Accessor indexAccess = indices.get();
 					final ByteBuffer indexBuffer = parser.getData(indexAccess);
-					loadFromIndex(attributesBuffer, vertexConsumerSupplier, buffer, indexAccess.getCount(), equalsOf(GLTF_COMPONENT_TYPE_UINT, indexAccess.getComponentType()) ? indexBuffer::getInt : indexBuffer::getShort);
+					loadFromIndex(attributesBuffer, vertexConsumerSupplier, buffer, indexAccess.getCount(), equalsOptional(GLTF_COMPONENT_TYPE_UINT, indexAccess.getComponentType()) ? indexBuffer::getInt : indexBuffer::getShort);
 				} else {
 					loadFromVertex(attributesBuffer, vertexConsumerSupplier, buffer, accessor.getCount());
 				}
@@ -181,7 +195,7 @@ public class GlTFModel implements IModelGeometry<GlTFModel> {
 		} else {
 			this.gltf.getNodes().forEach(node -> addNode(node, consumer));
 		}
-		ImmutableList.Builder<BakedQuad> bakedBuilder = ImmutableList.builder();
+		final ImmutableList.Builder<BakedQuad> bakedBuilder = ImmutableList.builder();
 		builderList.forEach(builder -> bakedBuilder.add(builder.build()));
 		return new GlTFBakedModel(bakedBuilder.build());
 	}
